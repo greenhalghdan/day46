92671d12ace7182e49604ca962b9d8ebaeba1751
danie <greenhalghdan@gmail.com>
2023-07-21 22:57:31 +0100

    scraping song titles

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ from bs4 import BeautifulSoup
 import requests
 
 
-year = input("what your would you like to go back to(YYYY-MM-DD)")
+#year = input("what your would you like to go back to(YYYY-MM-DD)")
 
 response = requests.get("https://www.billboard.com/charts/hot-100/2000-08-12")
 
+webpage = response.text
+
+soup = BeautifulSoup(webpage, "html.parser")
+
+songs = soup.find_all("div", class_="o-chart-results-list-row-container")
+a = soup.find("span", class_="c-label a-font-primary-bold-l u-font-size-32@tablet u-letter-spacing-0080@tablet")
+b = soup.find_all("h3", id="title-of-a-story")
+ranking = []
+song_name = []
+for song in songs:
+    #ranking.append(song.find("span", class_="c-label a-font-primary-bold-l u-font-size-32@tablet u-letter-spacing-0080@tablet").getText().split())
+    rank = (song.find("span",
+                             class_="c-label a-font-primary-bold-l u-font-size-32@tablet u-letter-spacing-0080@tablet").getText()).split()
+    song_name.append(song.find("h3", id="title-of-a-story").getText().strip())
+    ranking.append(int(int(rank[0])))
